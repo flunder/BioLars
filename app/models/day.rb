e022560f0b948cc1d12a2d5641a20918a2a949bd
flunder <larsf2005@gmail.com>
2010-11-24 20:18:14 +0000

    lots of partials

--- app/models/day.rb
+++ app/models/day.rb
@@ -1,4 +1,5 @@
 class Day < ActiveRecord::Base
-  #validates_uniqueness_of :date
+  validates_uniqueness_of :date
   validates_presence_of :field1, :field2, :field3, :field4, :field5
+
 end
--- public/javascripts/bio.js
+++ public/javascripts/bio.js
@@ -1,7 +1,21 @@
+$(document).ready(function() {
+    $('.jqueryToggleBtn').addClass('unselectable');
+    $('.jqueryToggleBtn').click(function() {
+        $(this).siblings('.jqueryToggleContent').toggle('slow');
+    })
+
+	  if ( $('.commentBox textarea').attr('value').length == 0) {
+			$('.commentBox textarea').attr('value','comment');
+		}
+
+});
 
 $(document).ready(function() {
-  	$('.jqueryToggleBtn').addClass('unselectable');
-		$('.jqueryToggleBtn').click(function(){
-			$(this).siblings('.jqueryToggleContent').toggle('slow');
-		})
+    $(".smartInput").focus(function() {
+        this.value = "";
+    }).blur(function() {
+        if (!this.value.length) {
+            this.value = this.defaultValue;
+        }
+    });
 });
